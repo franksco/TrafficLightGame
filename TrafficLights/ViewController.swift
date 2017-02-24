e33d2b26841f6e7e86e102dd3ea2dd506f7a0bee
franksco <franksco39@gmail.com>
2017-02-23 19:33:52 -0800

    added variables for timer, scoreTimer, timerInt, scoreInt. set scoreInt to equal 0 on load and display as 0. wrote func for StartStop button, if score Int equals 0 timerInt will start at 3 and display the correct trafficLight image and will continue to count every second. Referencing updateCounter func which tells it which image to displaybased on what the timerInt is saying.

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -14,18 +14,60 @@ class ViewController: UIViewController {
     @IBOutlet var startStopButton: UIButton!
     @IBOutlet var counterLabel: UILabel!
     
+    var timer = Timer()
+    var scoreTimer = Timer()
+    
+    var timerInt = 0
+    var scoreInt = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scoreInt = 0
+        counterLabel.text =  String(scoreInt)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
     @IBAction func StartStop(_ sender: Any) {
+        
+        if scoreInt == 0 {
+            
+            timerInt = 3
+            
+            trafficLight.image = UIImage(named: "TrafficLight")
+            
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateCounter), userInfo: nil, repeats: true)
+            
+        }
+        
+    }
+    
+    func updateCounter() {
+    
+        timerInt -= 1
+        
+        if timerInt == 2 {
+            
+            trafficLight.image = UIImage(named: "TrafficLight3")
+            
+        } else if timerInt == 1 {
+            
+            trafficLight.image = UIImage(named: "TrafficLight2")
+            
+        } else if timerInt == 0 {
+            
+            trafficLight.image = UIImage(named: "TrafficLight1")
+            
+            timer.invalidate()
+            
+        }
     }
 
 }
-
